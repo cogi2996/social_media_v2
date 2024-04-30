4ffdb76b2bc472128b61043199097884d2ecb69c
cogi2996 <bidang.tuyloan@gmail.com>
2024-05-01 00:09:31 +0700

    bug: X-Content-type-option not set with firebase

--- src/main/resources/static/assets/js/Interceptor/intercept.js
+++ src/main/resources/static/assets/js/Interceptor/intercept.js
@@ -1,14 +1,14 @@
 axios.interceptors.request.use(
   function (request) {
     // Đính token vào header mới
-    if (request.method !== 'get') {
-        const { header, token } = getCSRFTOKEN();
-        const newHeaders = {
-              ...request.headers,
-               [header]: token,
-        };
-        // override with new headers
-        request.headers = newHeaders;
+    if (request.method !== "get") {
+      const { header, token } = getCSRFTOKEN();
+      const newHeaders = {
+        ...request.headers,
+        [header]: token,
+      };
+      // override with new headers
+      request.headers = newHeaders;
     }
 
     return request;
@@ -20,14 +20,18 @@ axios.interceptors.request.use(
 );
 
 function getCSRFTOKEN() {
-    // get token protect csrf from meta tag each view thymeleaf ( layout )
-    let token = document.querySelector("meta[name='_csrf']").getAttribute("content");
-    let header = document.querySelector("meta[name='_csrf_header']").getAttribute("content");
-    return { header, token };
+  // get token protect csrf from meta tag each view thymeleaf ( layout )
+  let token = document
+    .querySelector("meta[name='_csrf']")
+    .getAttribute("content");
+  let header = document
+    .querySelector("meta[name='_csrf_header']")
+    .getAttribute("content");
+  return { header, token };
 }
 
-
 axios.interceptors.response.use(
+  // add X-Content-Type-Options if response don't have
   function (response) {
     // Thực thi các kịch bản cần thiết ở đây
     // trước khi response đến điểm cuối
